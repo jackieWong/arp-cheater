cf0dba831f179ddf0ed60ae019c8b51759262c15
wstnap <wstnap@gmail.com>
2014-10-10 13:58:31 +0800

    print the default interface

--- arp.c
+++ arp.c
@@ -1,8 +1,16 @@
-#include <pcap.h>
+#include <pcap/pcap.h>
 
 int main(int argc, char **argv)
 {
-	char *dev = argv[1];
+	//char *dev = argv[1];
+	//printf("Device: %s\n", dev);
+	char *dev, errbuf[PCAP_ERRBUF_SIZE];
+	dev = pcap_lookupdev(errbuf);
+
+	if(dev == NULL) {
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+	}
+
 	printf("Device: %s\n", dev);
 	return 0;
 }
